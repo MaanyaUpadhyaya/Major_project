c7feed8751b6f30e2dbbd040709b0102188b0d4a
Maanya Upadhyaya <maanyaupadhyaya@Maanyas-MacBook-Pro.local>
2024-04-03 20:10:55 +0530

    report version

--- disease_prediction.py
+++ disease_prediction.py
@@ -5,6 +5,7 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+import joblib
 
 class DiseasePredictor:
     def __init__(self):
@@ -25,14 +26,17 @@ class DiseasePredictor:
         X_train, _, y_train, _ = train_test_split(X, y, test_size=0.2, random_state=42)
         for name, model in self.models.items():
             model.fit(X_train, y_train)
+            # Save the model as a pickle file
+            joblib.dump(model, f'{name}_model.pkl')
         self.label_encoder.fit(y)
 
     def predict_disease(self, symptoms):
         encoded_symptoms = self.label_encoder.transform(symptoms)
         predictions = {}
         for name, model in self.models.items():
+            # Load the model from the file
+            model = joblib.load(f'{name}_model.pkl')
             encoded_symptoms_2d = encoded_symptoms.reshape(1, -1)
             prediction = model.predict(encoded_symptoms_2d)
             predictions[name] = self.label_encoder.inverse_transform(prediction)[0]
         return predictions
-
